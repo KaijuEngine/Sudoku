7d472a28a32d123ae5d956113074cb8f717550d2
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2026-02-13 21:16:26 -0600

    Fixing accidental content config deletion

--- src/entity_data_binding_init.go
+++ src/entity_data_binding_init.go
@@ -9,4 +9,5 @@ import (	_ "kaiju/engine_entity_data/engine_entity_data_audio"
 	_ "kaiju/engine_entity_data/engine_entity_data_physics"
 	_ "kaiju/engine_entity_data/engine_entity_data_skin_animation"
 	_ "kaiju/engine_entity_data/engine_entity_data_tween_transform"
+	_ "game/main_menu"
 )
